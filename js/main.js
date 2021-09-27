ab537da81f6af28bd2b4be32e97a0d74aa800bcb
Ksenia Zhukova <ks.jukova6@gmail.com>
2021-09-27 22:27:16 +0300

    Убрала console.log

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ function getRandomInteger (min, max) {
   if (max > min && min >= 0) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  console.log ('Указан неправильный диапазон');
   return -1;
 }
 getRandomInteger (1, 3);
@@ -12,7 +11,6 @@ function getRandomFP (min, max, demicalPlaces) {
   if (max > min && min >= 0) {
     return (Math.random() * (max - min) + min).toFixed(demicalPlaces);
   }
-  console.log ('Указан неправильный диапазон');
   return -1;
 }
 getRandomFP (1, 2, 2);
