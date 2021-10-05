54c71140e0368b257b8a5613d8828546a41b8895
Ksenia Zhukova <ks.jukova6@gmail.com>
2021-10-05 21:50:28 +0300

    Внесла правки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+const AVATAR_IDS = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+
+const TITLES = [
+  'Дом под старину',
+  'С видом на сад',
+  'Домик с картинки',
+  'Для большой семьи',
+  'Дом с самым необычным дизайном',
+  'Безупречно даже в деталях',
+  'Легендарные стиль и качество',
+  'Компактность, практичность и экономия',
+  'Карнавал текстур и красок',
+  'Комнаты с высокими потолками'];
+
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+
+const CHECKIN_VARIANTS = ['12:00', '13:00', '14:00'];
+
+const CHECKOUT_VARIANTS = ['12:00', '13:00', '14:00'];
+
+const FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+const DESCRIPTIONS = [
+  'самая низкая цена среди подобных',
+  'современный дорогостоящий ремонт, не требующий вложений',
+  'удобное местоположение',
+  'редкая и практичная планировка',
+  'в подарок остается новый итальянский кухонный гарнитур',
+  'панорамный вид на город',
+  'панорамный вид на озеро',
+  'очень уютно',
+  'торговый центр в швговой доступности',
+  'лучшее предложение'];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+const SIMILAR_ADVERT_COUNT = 10;
+
 // Источник: https://learn.javascript.ru/task/random-int-min-max
 const getRandomInteger = (min, max) => {
   if (max > min && min >= 0) {
@@ -13,90 +54,47 @@ const getRandomFloat = (min, max, decimalPlaces) => {
   }
   throw new Error('Invalid argument: value min cannot be less than zero, value max must be greater than value min.');
 };
-const getRandomArrayElement = (elements) => {
-  const RANDOM_INDEX = getRandomInteger(0, elements.length - 1);
-  return elements[RANDOM_INDEX];
-};
 
-const AVATAR_IDS = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'];
+const getRandomArrayElement = (elements) => {
+  elements[getRandomInteger(0, elements.length - 1)];
+};
 
 const getRandomAvatarURL = () => {
-  const RANDOM_AVATAR_INDEX = (AVATAR_IDS.length === 1) ? 0 : getRandomInteger(0, AVATAR_IDS.length - 1);
-  const RESULT = `img/avatars/user${AVATAR_IDS[RANDOM_AVATAR_INDEX]}.png`;
-  AVATAR_IDS.splice(RANDOM_AVATAR_INDEX, 1);
-  return RESULT;
+  const randomAvatarIndex = (AVATAR_IDS.length === 1) ? 0 : getRandomInteger(0, AVATAR_IDS.length - 1);
+  const result = `img/avatars/user${AVATAR_IDS[randomAvatarIndex]}.png`;
+  AVATAR_IDS.splice(randomAvatarIndex, 1);
+  return result;
 };
-
-const TITLES = [
-  'Дом под старину',
-  'С видом на сад',
-  'Домик с картинки',
-  'Для большой семьи',
-  'Дом с самым необычным дизайном',
-  'Безупречно даже в деталях',
-  'Легендарные стиль и качество',
-  'Компактность, практичность и экономия',
-  'Карнавал текстур и красок',
-  'Комнаты с высокими потолками'];
 
 const getRandomLocation = () => {
   const result = {lat: getRandomFloat (35.65000, 35.70000, 5), lng: getRandomFloat (139.70000, 139.80000, 5)};
   return result;
 };
 
-const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-
-const CHECKIN_VARIANTS = ['12:00', '13:00', '14:00'];
-
-const CHECKOUT_VARIANTS = ['12:00', '13:00', '14:00'];
-
-const FEATURES_LIST = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-
 const getRandomSubarray = (elements) => {
-  const ARR_LEN = getRandomInteger(1, elements.length);
-  const USED_INDEXES = [];
-  const RESULTS = [];
-  if (ARR_LEN === elements.length) {
+  const arrLen = getRandomInteger(1, elements.length);
+  const usedIndexes = [];
+  const results = [];
+  if (arrLen === elements.length) {
     return elements;
   }
-  while (RESULTS.length !== ARR_LEN) {
-    const INDEX = getRandomInteger(0, elements.length - 1);
-    if (USED_INDEXES.includes(INDEX)) {
+  while (results.length !== arrLen) {
+    const index = getRandomInteger(0, elements.length - 1);
+    if (usedIndexes.includes(index)) {
       continue;
     }
-    RESULTS.push(elements[INDEX]);
-    USED_INDEXES.push(INDEX);
+    results.push(elements[index]);
+    usedIndexes.push(index);
   }
-  return RESULTS;
+  return results;
 };
 
-const DESCRIPTIONS = [
-  'самая низкая цена среди подобных',
-  'современный дорогостоящий ремонт, не требующий вложений',
-  'удобное местоположение',
-  'редкая и практичная планировка',
-  'в подарок остаются новый итальянский кухонный гарнитур',
-  'панорамный вид на город',
-  'панорамный вид на озеро',
-  'очень уютно',
-  'торговый центр в швговой доступности',
-  'лучшее предложение'];
-
-const PHOTOS = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-
-const SIMILAR_ADVERT_COUNT = 10;
-
 const createAdvert = () => {
-  const LOCATION = getRandomLocation();
-
-  const AUTHOR = {AVATAR: getRandomAvatarURL()};
-
-  const OFFER = {
+  const location = getRandomLocation();
+  const author = {AVATAR: getRandomAvatarURL()};
+  const offer = {
     title: getRandomArrayElement(TITLES),
-    address: `${LOCATION.lat}, ${LOCATION.lng}`,
+    address: `${location.lat}, ${location.lng}`,
     price: getRandomInteger (1, 100000000000),
     type: getRandomArrayElement(TYPES),
     rooms: getRandomInteger (1, 100),
@@ -107,13 +105,17 @@ const createAdvert = () => {
     description: getRandomArrayElement(DESCRIPTIONS),
     photos: getRandomSubarray(PHOTOS),
   };
-
   return  {
-    author: AUTHOR,
-    offer: OFFER,
-    location: LOCATION,
+    author: author,
+    offer: offer,
+    location: location,
   };
 };
 
-const similarAdverts = Array.from({length: SIMILAR_ADVERT_COUNT}, createAdvert);
+const createRandomAdvert = () => {
+  Array.from({length: SIMILAR_ADVERT_COUNT}, createAdvert);
+};
+
+const similarAdverts = createRandomAdvert();
+
 console.log(similarAdverts); //eslint-disable-line no-console
