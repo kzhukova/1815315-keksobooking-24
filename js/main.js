8ae1020f9dfc4b3d7cfbb7f2fd481fe772c972f1
Ksenia Zhukova <ks.jukova6@gmail.com>
2021-10-31 12:07:21 +0300

    Отрисуй меня полностью (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import {createRandomAdverts} from './mock.js';
+// import {createRandomAdverts} from './mock.js';
+import './template.js';
 
-const similarAdverts = createRandomAdverts();
-
-console.log(similarAdverts); //eslint-disable-line no-console
+// const similarAdverts = createRandomAdverts();
+// console.log(similarAdverts); //eslint-disable-line no-console
