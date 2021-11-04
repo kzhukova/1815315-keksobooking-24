8d4708deb33e8a0262d65f0785d7bfcafec681f9
Ksenia Zhukova <ks.jukova6@gmail.com>
2021-11-04 15:49:54 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import {createRandomAdverts} from './mock.js';
 import {renderMap} from './popup.js';
-import {disablePage, activatePage} from './toggle_activation.js';
+import {disablePage, enablePage} from './toggle-activation.js';
 
 const similarAdverts = createRandomAdverts();
 renderMap(similarAdverts[0]);
-activatePage();
+enablePage();
 disablePage();
--- js/toggle-activation.js
+++ js/toggle-activation.js
@@ -1,37 +1,39 @@
 const disablePage = () => {
   const adFormElement = document.querySelector('.ad-form');
   adFormElement.classList.add('add-form--disabled');
-  for (const el of adFormElement.getElementsByTagName('fieldset')) {
+  adFormElement.querySelector('.ad-form-header__input').disabled = true;
+  for (const el of adFormElement.querySelectorAll('.ad-form__element')) {
     el.disabled = true;
   }
 
   const mapFiltersElement = document.querySelector('.map__filters');
   mapFiltersElement.classList.add('add-form--disabled');
-  for (const el of mapFiltersElement.getElementsByTagName('select')) {
+  for (const el of mapFiltersElement.querySelectorAll('.map__filter')) {
     el.disabled = true;
   }
-  for (const el of mapFiltersElement.getElementsByTagName('fieldset')) {
+  for (const el of mapFiltersElement.querySelectorAll('.map__checkbox')) {
     el.disabled = true;
   }
 
 };
 
-const activatePage = () => {
+const enablePage = () => {
   const adFormElement = document.querySelector('.ad-form');
   adFormElement.classList.remove('add-form--disabled');
-  for (const el of adFormElement.getElementsByTagName('fieldset')) {
+  adFormElement.querySelector('.ad-form-header__input').disabled = false;
+  for (const el of adFormElement.querySelectorAll('.ad-form__element')) {
     el.disabled = false;
   }
 
   const mapFiltersElement = document.querySelector('.map__filters');
   mapFiltersElement.classList.remove('add-form--disabled');
-  for (const el of mapFiltersElement.getElementsByTagName('select')) {
+  for (const el of mapFiltersElement.querySelectorAll('.map__filter')) {
     el.disabled = false;
   }
-  for (const el of mapFiltersElement.getElementsByTagName('fieldset')) {
+  for (const el of mapFiltersElement.querySelectorAll('.map__checkbox')) {
     el.disabled = false;
   }
 
 };
-export {disablePage, activatePage};
+export {disablePage, enablePage};
 
