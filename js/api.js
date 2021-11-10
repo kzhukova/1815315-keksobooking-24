aeb96c75173524ca71a63842dcccd52053a1529c
Ksenia Zhukova <ks.jukova6@gmail.com>
2021-11-10 14:26:56 +0300

    надо прокачатся

---
+++ js/api.js
@@ -0,0 +1,40 @@
+const fetchAdverts =() => {
+  const res = fetch(
+    'https://24.javascript.pages.academy/keksobooking/data',
+    {
+      method: 'GET',
+      credentials: 'same-origin',
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(`${response.status} ${response.statusText}`);
+    });
+  return res;
+};
+
+
+const sendNewAdvert = (onSuccess, onFail, body) => {
+  const res = fetch(
+    'https://24.javascript.pages.academy/keksobooking',
+    {
+      method: 'POST',
+      body,
+    },
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        onFail();
+      }
+    })
+    .catch(() => {
+      onFail();
+    });
+  return res;
+};
+
+export {fetchAdverts, sendNewAdvert};
--- js/main.js
+++ js/main.js
@@ -1,8 +1,5 @@
-
 import { addListenersToForm } from './form.js';
 import {renderMap} from './map.js';
-import {createRandomAdverts} from './mock.js';
 
 addListenersToForm();
-const adverts = createRandomAdverts();
-renderMap(adverts);
+renderMap();
